17c0306d19b3de671e92cad6f95e8e347858b072
tondablatnik11 <tondablatnik11@gmail.com>
2025-07-15 23:09:57 +0200

    Fina8

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 'use client';
-import AppProviders from '@/components/layout/AppProviders';
 import DashboardLayout from '@/components/layout/DashboardLayout';
 import Login from '@/components/auth/Login';
 import { useAuth } from '@/hooks/useAuth';
 import { Toaster } from 'react-hot-toast';export default function Home() {
-const { user, loading } = useAuth();if (loading) {
+const { user, loading } = useAuth();// Zobrazí se, dokud se ověřuje přihlášení
+if (loading) {
 return (
 
 Načítání...
