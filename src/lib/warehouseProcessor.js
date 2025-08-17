38f9d416f5478c900918d0f711ae6aea9d6f3734
tondablatnik11 <tondablatnik11@gmail.com>
2025-08-17 17:45:48 +0200

    Sklad_v2_20

--- src/lib/warehouseProcessor.js
+++ src/lib/warehouseProcessor.js
@@ -48,6 +48,7 @@ export const createWarehouseSnapshot = (layoutData, stockData) => {
 
         const labelKey = `${haus}-${regal}`;
         if (!labelData.has(labelKey)) {
+            // Umístění popisku před daný regál
             labelData.set(labelKey, { text: `R${regal}`, position: [x, 0.01, minZ - RACK_DEPTH * 2.5] });
         }
 
